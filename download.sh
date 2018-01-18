b56a8fb89247db0e3f319f1239cc6d8f5ed1c476
John Wu <topjohnwu@gmail.com>
2018-01-18 21:50:29 +0800

    Update download.sh
    Change file host to Github release

--- download.sh
+++ download.sh
@@ -1,4 +1,1 @@
-git clone https://gitlab.com/cartercheng34/MLDS_final.git
-mv MLDS_final/full_yolo_hand_small.h5 .
-mv MLDS_final/model .
-rm -rf MLDS_final
+curl -L https://github.com/NTUwanderer/MLDS_106-1_final/releases/download/models_dl/models.tar.gz | tar zxf -
